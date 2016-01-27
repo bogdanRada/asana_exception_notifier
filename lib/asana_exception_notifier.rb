cddf50d929ac64e49461575cebe917cbec4e88f0
Rada Bogdan Raul <raoul_ice@yahoo.com>
2016-01-27 20:31:07 +0200

    update

--- lib/asana_exception_notifier.rb
+++ lib/asana_exception_notifier.rb
@@ -9,7 +9,4 @@ require 'erb'
 require 'tilt'
 require 'tilt/erb'
 
-require 'ostruct'
-require 'json'
 Gem.find_files('asana_exception_notifier/**/*.rb').each { |path| require path }
-Gem.find_files('exception_notifier/**/*.rb').each { |path| require path }
--- lib/asana_exception_notifier/helper.rb
+++ lib/asana_exception_notifier/helper.rb
@@ -5,12 +5,12 @@ module AsanaExceptionNotifier
 
     module_function
 
-    def extract_body(env)
-      if io = env['rack.input']
-        io.rewind if io.respond_to?(:rewind)
-        io.read
-      end
-    end
+    # def extract_body(env)
+    #   if io = env['rack.input']
+    #     io.rewind if io.respond_to?(:rewind)
+    #     io.read
+    #   end
+    # end
 
     def show_hash_content(hash)
       hash.map do |key , value|
@@ -36,13 +36,6 @@ module AsanaExceptionNotifier
       end
     end
 
-    # Returns utf8 encoding of the msg
-    # @param [String] msg
-    # @return [String] ReturnsReturns utf8 encoding of the msg
-    def force_utf8_encoding(msg)
-      msg.respond_to?(:force_encoding) && msg.encoding.name != 'UTF-8' ? msg.force_encoding('UTF-8') : msg
-    end
-
     # Dispatches the response either to the final callback or to the block that will use the response
     # and then call the callback
     #
@@ -55,16 +48,6 @@ module AsanaExceptionNotifier
       (!res.empty? && !callback.nil?) ? callback.call(res) : block.call(res)
     end
 
-
-    # Method that is used to parse a string as JSON , if it fails will return nil
-    # @see JSON#parse
-    # @param [string] res The string that will be parsed as JSON
-    # @return [Hash, nil] Returns Hash object if the json parse succeeds or nil otherwise
-    def parse_json(res)
-      JSON.parse(res)
-    rescue JSON::ParserError
-      nil
-    end
 
 
   end
