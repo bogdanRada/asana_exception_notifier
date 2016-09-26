cc1e8fdb567fc15e3f726e77b9e3fe7bd13e9c5c
Rada Bogdan Raul <raoul_ice@yahoo.com>
2016-09-26 12:04:55 +0300

    update travis

--- lib/asana_exception_notifier/version.rb
+++ lib/asana_exception_notifier/version.rb
@@ -16,7 +16,7 @@ module AsanaExceptionNotifier
     # major release version
     MAJOR = 0
     # minor release version
-    MINOR = 5
+    MINOR = 6
     # tiny release version
     TINY = 0
     # prelease version ( set this only if it is a prelease)
