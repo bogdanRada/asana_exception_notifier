df533dd36bf89f34c293b382c766fe4e0cea2fd8
Rada Bogdan Raul <raoul_ice@yahoo.com>
2016-12-07 07:56:16 +0200

    update dev dependencies to rake

--- lib/asana_exception_notifier/version.rb
+++ lib/asana_exception_notifier/version.rb
@@ -16,7 +16,7 @@ module AsanaExceptionNotifier
     # major release version
     MAJOR = 0
     # minor release version
-    MINOR = 7
+    MINOR = 8
     # tiny release version
     TINY = 0
     # prelease version ( set this only if it is a prelease)
